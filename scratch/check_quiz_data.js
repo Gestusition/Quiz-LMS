a2e6f2abe58026f5741505937b703f7fc8129eb0
Gestusition <polatkankirac@gmail.com>
2026-05-15 11:41:09 +0300

    chore: add scratch directory and test_output.txt to gitignore

--- scratch/check_quiz_data.js
+++
@@ -1,9 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const path = require('path');
-
-const dbPath = path.join(__dirname, '..', 'data', 'quiz.assessment.sqlite');
-const db = new DatabaseSync(dbPath);
-
-const quiz = db.prepare("SELECT * FROM quizzes WHERE title LIKE '%Programming Basics%'").get();
-console.log('Quiz Data:', JSON.stringify(quiz, null, 2));
-console.log('Weight Type:', typeof quiz.weight);
